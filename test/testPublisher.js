08a322f3bd5efa58de8ff9eab7b2018db7d00485
Frederic Heem <frederic.heem@gmail.com>
2015-05-14 23:12:05 +0100

    forgot testPublisher.js

---
+++ test/testPublisher.js
@@ -0,0 +1,34 @@
+//var assert = require('assert');
+//var Promise = require('bluebird');
+var Publisher = require('../lib/publisher');
+
+describe('Publisher', function() {
+  'use strict';
+  this.timeout(15e3);
+  var log = require('logfilename')(__filename, {
+    console: {
+      level: 'debug'
+    }
+  });
+
+  log.debug('');
+  var publisher;
+
+  var publisherOptions = {
+    exchange: 'user'
+  };
+
+  describe('StartStop', function() {
+    it('should start and stop the publisher', function(done) {
+      publisher = new Publisher(publisherOptions);
+      publisher.start().delay(1e3).then(publisher.stop).then(done,
+        done);
+    });
+    it('should stop the publisher without start', function(done) {
+      publisher = new Publisher(publisherOptions);
+      publisher.stop().then(done, done);
+    });
+
+  });
+
+});
